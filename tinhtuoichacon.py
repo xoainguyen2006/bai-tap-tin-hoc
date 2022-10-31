50a542652234d78c49b896c82604048e593b8dab
xoainguyen2006 <110514018+xoainguyen2006@users.noreply.github.com>
2022-10-31 21:47:51 +0700

    Update tinhtuoichacon.py

--- tinhtuoichacon.py
+++ tinhtuoichacon.py
@@ -1,10 +1,10 @@
-def main():
+cha = int(input('So tuoi cua cha: '))
+con = int(input('So tuoi cua con: '))
+
+while con<0 or (con*2>=cha) or (con+15>cha):
+    print('Tuoi cha va tuoi con khong dung voi yeu cau de bai')
+    print('Nhap lai!')
     cha = int(input('So tuoi cua cha: '))
     con = int(input('So tuoi cua con: '))
-    if con<0 or (con*2>=cha) or (con+15>cha):
-        print('Tuoi cha va tuoi con khong dung voi de bai')
-        print('Nhap lai!')
-        main();
-    else:
-        print('Sau', cha-con*2, 'nam nua thi tuoi cha gap doi tuoi con')
-main();
+
+print(f'Sau {cha-con*2} nam nua thi tuoi cha gap doi tuoi con')
